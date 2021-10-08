ac9a0f19cbed6741bac00594919011227e70e7d2
Denis-Neagu <denis.neagu@outlook.com>
2021-10-08 14:12:41 +0100

    refactored insert employee into database method

--- src/main/java/com/sparta/database/DatabaseController.java
+++ src/main/java/com/sparta/database/DatabaseController.java
@@ -36,27 +36,44 @@ public class DatabaseController {
     public void insertEmployee(List<Employee> employees, String tableName) {
         String insertIntoTable = "INSERT INTO "+"csv_migration."+tableName+"(`EMP_ID`, `NAME_PREFIX`, `FIRST_NAME`, `MIDDLE_INITIAL`, `LAST_NAME`, `GENDER`, `EMAIL`, `DATE_OF_BIRTH`, `DATE_OF_JOINING`, `SALARY`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+
+
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(insertIntoTable);
             connection.setAutoCommit(false);
 
             for (Employee employee : employees) {
-                preparedStatement.setInt(1, employee.getEmployeeID());
-                preparedStatement.setString(2, employee.getNamePrefix());
-                preparedStatement.setString(3, employee.getFirstName());
-                preparedStatement.setString(4, String.valueOf(employee.getMiddleInitial()));
-                preparedStatement.setString(5, employee.getLastName());
-                preparedStatement.setString(6, String.valueOf(employee.getGender()));
-                preparedStatement.setString(7, employee.getEmail());
-                preparedStatement.setDate(8, employee.getDateOfBirth());
-                preparedStatement.setDate(9, employee.getDateOfJoining());
-                preparedStatement.setDouble(10, employee.getSalary());
 
-                preparedStatement.execute();
+                String queryCheck = "SELECT * from "+tableName+" WHERE EMP_ID = ?";
+
+                PreparedStatement preparedStatement1 = connection.prepareStatement(queryCheck);
+                preparedStatement1.setInt(1,employee.getEmployeeID());
+
+                ResultSet rs = preparedStatement1.executeQuery();
+
+                if(rs.next()) {
+                    System.out.println(employee.getFirstName() + " " + employee.getLastName() + " Already exists in table " + tableName);
+                } else {
+                    preparedStatement.setInt(1, employee.getEmployeeID());
+                    preparedStatement.setString(2, employee.getNamePrefix());
+                    preparedStatement.setString(3, employee.getFirstName());
+                    preparedStatement.setString(4, String.valueOf(employee.getMiddleInitial()));
+                    preparedStatement.setString(5, employee.getLastName());
+                    preparedStatement.setString(6, String.valueOf(employee.getGender()));
+                    preparedStatement.setString(7, employee.getEmail());
+                    preparedStatement.setDate(8, employee.getDateOfBirth());
+                    preparedStatement.setDate(9, employee.getDateOfJoining());
+                    preparedStatement.setDouble(10, employee.getSalary());
+
+                    preparedStatement.execute();
+
+                    System.out.println(employee.getFirstName() + " " + employee.getLastName() + " successfully stored in " + tableName);
+
+                }
+
+
             }
             connection.commit();
-
-            System.out.println(tableName + " data successfully stored");
 
         } catch (SQLException e) {
             e.printStackTrace();
