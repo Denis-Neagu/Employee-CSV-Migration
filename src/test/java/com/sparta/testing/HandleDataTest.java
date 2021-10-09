4bb6307110da266e279ace441052cb272cbe2f30
Denis-Neagu <denis.neagu@outlook.com>
2021-10-09 20:18:40 +0100

    added unit tests for HandleData class

---
+++ src/test/java/com/sparta/testing/HandleDataTest.java
@@ -0,0 +1,113 @@
+package com.sparta.testing;
+
+import com.sparta.data.Employee;
+import com.sparta.data.HandleData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandleDataTest {
+    HandleData handleData;
+
+    @BeforeEach
+    public void setUp() {
+        handleData = new HandleData();
+    }
+
+    @Test
+    public void getDataFromCSV_ChecksDifferentEmployeeIDS() {
+        List<Employee> employeeList = handleData.getDataFromCSV("EmployeeRecordsLarge.csv");
+        Employee e1 = employeeList.get(0);
+        Employee e2 = employeeList.get(1);
+
+        assertNotEquals(e1, e2);
+    }
+
+    @Test
+    public void getDataFromCSV_DoesItReturnsDataAsList() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecordsLarge.csv");
+        ArrayList<Employee> employeeArrayList2 = handleData.getDataFromCSV("EmployeeRecords.csv");
+
+        assertNotEquals(employeeArrayList1, employeeArrayList2);
+    }
+
+    @Test
+    public void getDataFromCSV_CheckIfItReturnsCorrectData() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        ArrayList<Employee> employeeArrayList2 = handleData.getDataFromCSV("EmployeeRecords.csv");
+
+        assertEquals(employeeArrayList1, employeeArrayList2);
+    }
+
+    @Test
+    public void isValid_CheckIfProperDataReturnsTrue() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecordsLarge.csv");
+        boolean tempBooleanValue = handleData.isValid(employeeArrayList1);
+
+        assertTrue(tempBooleanValue);
+    }
+
+    @Test
+    public void isValid_CheckIfCorruptDataInEmployeeID() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        Employee employee0 = employeeArrayList1.get(0);
+        employee0.setEmployeeID(-30);
+
+        assertFalse(handleData.isValid(employeeArrayList1));
+    }
+
+    @Test
+    public void isValid_CheckIfCorruptDataInEmail() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        Employee employee0 = employeeArrayList1.get(0);
+        employee0.setEmail("thisisthebestemailevertobehonest");
+
+        assertFalse(handleData.isValid(employeeArrayList1));
+    }
+
+    @Test
+    public void isValid_CheckIfCorruptedDataInFirstName() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        Employee employee0 = employeeArrayList1.get(0);
+        employee0.setFirstName("denis...");
+
+        assertFalse(handleData.isValid(employeeArrayList1));
+    }
+
+    @Test
+    public void isValid_CheckIfCorruptDataInLastName() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        Employee employee0 = employeeArrayList1.get(0);
+        employee0.setLastName("neagu....---");
+
+        assertFalse(handleData.isValid(employeeArrayList1));
+    }
+
+    @Test
+    public void isValid_CheckIfCorruptDataInDateOfBirth() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        Employee employee0 = employeeArrayList1.get(0);
+
+        String tempDateString = "1999-05-28";
+        Date date = Date.valueOf(tempDateString);
+
+        employee0.setDateOfBirth(date);
+
+        assertTrue(handleData.isValid(employeeArrayList1));
+    }
+
+    @Test
+    public void isValid_CheckIfCorruptDataInGender() {
+        ArrayList<Employee> employeeArrayList1 = handleData.getDataFromCSV("EmployeeRecords.csv");
+        Employee employee0 = employeeArrayList1.get(0);
+        employee0.setGender('W');
+
+        assertFalse(handleData.isValid(employeeArrayList1));
+    }
+
+}
